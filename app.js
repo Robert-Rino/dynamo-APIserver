8377233455bcb5d1b567db540abc6ef42b4b4a7e
Robert-Rino <a901002666@gmail.com>
2016-01-12 04:44:19 +0800

    update

--- app.js
+++ app.js
@@ -5,65 +5,44 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let expressJwt = require('express-jwt');
+let cors = require('cors');
 const config = require('config');
+const hasJWTSecret = config.has('JWT.secret');
 
 let routes = require('./routes/index');
+let auth = require('./routes/auth.js');
 let users = require('./routes/users');
 
+let authenticate = hasJWTSecret ?
+  expressJwt({secret: config.get('JWT.secret')}):
+  expressJwt({secret: process.env.JWT_SECRET});
 let app = express();
 
-// app.use(cors());
-// app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// let dbConfig = config.get('Customer.dbConfig');
-// console.log(dbConfig.dbName)
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', routes);
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// return 401 when no authorization
+app.use('/api', authenticate, (err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Unauthorized or invalid token.');
+  }
+});
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use('/', auth);
+app.use('/api/v1', routes);
 app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
