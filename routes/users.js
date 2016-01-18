d847f98fa574d282b38a75cfa39dcebc6e106f34
Robert-Rino <a901002666@gmail.com>
2016-01-18 13:44:09 +0800

    add function only get latest record

--- routes/users.js
+++ routes/users.js
@@ -25,20 +25,43 @@ let data = {
 },
 };
 
-/* GET users listing. */
+/* GET users testing. */
 router.get('/', function(req, res) {
   res.send('user.js page').status(200);
 });
 
-/* GET users listing. */
+/* GET latest users record ). */
+router.get('/read/playrecord/:uid/:cid', function(req, res) {
+  let userId = parseInt(req.params.uid);
+  let courseId = parseInt(req.params.cid);
+
+  uPlayModel
+    .query(userId)
+    .filter('courseId').equals(courseId)
+    .descending()
+    .limit(1)
+    .exec((err, resp) => {
+      if (err) {
+        debug(err);
+        console.log('Error running query', err);
+      } else {
+        // console.log(_.pluck(resp.Items, 'attrs'));
+        res.status(200).send(_.pluck(resp.Items, 'attrs'));
+      }
+    });
+});
+
+/* GET all users record begin with time(Linux time format => moment().unix()). */
 router.get('/read/playrecord/:uid/:cid/:time', function(req, res) {
   let userId = parseInt(req.params.uid);
   let courseId = parseInt(req.params.cid);
   let timestamp = parseInt(req.params.time);
+
   uPlayModel
     .query(userId)
     .where('timestamp').gt(timestamp)
     .filter('courseId').equals(courseId)
+    .loadAll()
     .exec((err, resp) => {
       if (err) {
         debug(err);
